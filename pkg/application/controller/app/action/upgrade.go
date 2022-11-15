3a309fdc73220c172407c978215d54277f751bc8
xdonggao <xdonggao@tencent.com>
2022-11-15 17:40:29 +0800

    fix: helm upgrade max history

--- pkg/application/controller/app/action/upgrade.go
+++ pkg/application/controller/app/action/upgrade.go
@@ -116,6 +116,7 @@ func Upgrade(ctx context.Context,
 			ChartPathOptions: chartPathBasicOptions,
 			Wait:             wait,
 			WaitForJobs:      wait,
+			MaxHistory:       10,
 		})
 		if err != nil {
 			if updateStatusFunc != nil {
